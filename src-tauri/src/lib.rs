3f3db8847efc5bfb93c73224186209912b4ce381
Filip <nachtman.filip@seznam.cz>
2025-04-06 20:26:57 +0200

    Updater

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -3,6 +3,7 @@ mod webshare;
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_updater::Builder::new().build())
         .invoke_handler(tauri::generate_handler![webshare::get_salt])
         .setup(|app| {
             if cfg!(debug_assertions) {
